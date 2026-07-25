472db37e6dd13d919b32ec4886c7e570699ac099
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2026-07-25 11:51:05 +0200

    Physics Rule Combinator

--- src/main/scala/engine/physics/combinators/RuleCombinators.scala
+++
@@ -1,3 +0,0 @@
-package engine.physics.combinators
-
-trait RuleCombinators 
--- src/main/scala/engine/physics/core/PhysicsError.scala
+++ src/main/scala/engine/physics/core/PhysicsError.scala
@@ -13,3 +13,6 @@ case class OutOfBoundEntity(position: Vector2D)
 
 case class PhysicsDomainError(cause: EngineError)
   extends PhysicsError(s"Physics update rejected by the domain model: $cause")
+
+case class PhysicsRuleError(cause: String)
+  extends PhysicsError(s"Physics rule failed: $cause")
