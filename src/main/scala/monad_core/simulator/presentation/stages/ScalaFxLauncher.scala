1d0221709ddb73572130d02d15bd3e6f1159221c
fabio <fabiofattoriw@gmail.com>
2026-08-25 21:55:30 +0200

    completed the stage directory scaladoc

--- src/main/scala/monad_core/simulator/presentation/stages/ScalaFxLauncher.scala
+++ src/main/scala/monad_core/simulator/presentation/stages/ScalaFxLauncher.scala
@@ -16,11 +16,31 @@ import scalafx.stage.Stage
 import java.util.concurrent.{CountDownLatch, TimeUnit}
 import scala.concurrent.ExecutionContext.Implicits.global
 
+/**
+ * Contains data that can alter the behavior of the startup routine
+ * @param timeoutSeconds the waited time before the startup routine execution expressed in seconds
+ */
 private case class BlockingJfxBootstrapData(timeoutSeconds: Long)
 
+/**
+ * Utility object to encapsulate the management of the ScalaFx UI Thread.
+ */
 private object BlockingJfxBootstrap:
 
-  def run(data: BlockingJfxBootstrapData)(
+  /**
+   * Run the provided action safely by trying to start the ScalaFx UI Thread,
+   * if it's already running the action is scheduled to start as soon as possible.
+   *
+   *
+   * @param data [[BlockingJfxBootstrapData]] containing startup behavior information
+   * @param action the executed function on the UI thread
+   * @return `Left(BaseError)` if any unexpected error was thrown by the ScalaFx library during startup
+   *
+   *         `Right(Unit)` if the action was executed correctly
+   */
+  def run(
+      data: BlockingJfxBootstrapData
+  )(
       action: () => Either[BaseError, Unit]
   ): Either[BaseError, Unit] =
     val timeoutSeconds = data.timeoutSeconds
@@ -46,12 +66,24 @@ private object BlockingJfxBootstrap:
 
     if !completedInTime then Left(StartupTimeout(timeoutSeconds)) else result
 
+/**
+ * ScalaFx Entry Point of the application, it wraps the UI Thread management and displays the result
+ * of [[MainStageBuilder.buildRootContent()]] provided into a [[Stage]] constructed by the [[buildStage()]] function.
+ *
+ * @see [[BlockingJfxBootstrap]]
+ * @param mainStage the builder who construct the displayed stage inside the main window
+ */
 final class ScalaFxLauncher(mainStage: MainStageBuilder):
 
   private val MinStageWidth         = 1024.0
   private val MinStageHeight        = 720.0
   private val StartupTimeoutSeconds = 10L
 
+  /**
+   * Build the [[Stage]] with the default values as minWidth and minHeight by [[MinStageWidth]] and [[MinStageHeight]]
+   *
+   * @return the built [[Stage]]
+   */
   private def buildStage(): Stage =
     new Stage {
       title = "MonadCore2D"
@@ -60,11 +92,28 @@ final class ScalaFxLauncher(mainStage: MainStageBuilder):
       minHeight = MinStageHeight
     }
 
+  /**
+   * Builds the [[Scene]] used as background and container of the [[MainStageBuilder.buildRootContent()]]
+   *
+   * @return the built [[Scene]]
+   */
   private def buildScene(): Scene =
     new Scene(900, 600) {
       fill = Color.rgb(25, 26, 28)
     }
 
+  /**
+   * Starts the ScalaFx UI Thread by invoking [[BlockingJfxBootstrap.run]], to which it provides an action
+   * that build the main window of the application.
+   *
+   * @see [[buildStage()]], [[buildScene()]], [[BlockingJfxBootstrap]]
+   * @param aiAgent The [[AiAgent]] that will be provided to the [[MainStageBuilder.buildRootContent()]]
+   * @param world The [[World]] that will be provided to the [[MainStageBuilder.buildRootContent()]]
+   * @param gameEngineRuntime The [[GameEngineRuntime]] that will be provided to the [[MainStageBuilder.buildRootContent()]]
+   * @return `Left(BaseError)` which is carried from [[BlockingJfxBootstrap.run()]] or [[MainStageBuilder.buildRootContent()]]
+   *         
+   *         `Right(Unit)` when the window is constructed correctly
+   */
   def run()(using
       aiAgent: AiAgent,
       world: World,
